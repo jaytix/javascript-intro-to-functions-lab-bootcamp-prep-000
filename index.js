f7822d4a3eb646e108274cdfd44e88503e629040
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-10-21 20:42:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   }
 }
 
-function sayHiToGrandma(string) {
+function sayHiToGranda(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
